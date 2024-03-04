0c463167778b4b0096261d828afd011b61e115a5
Amanda Fox <Amanda.Fox02@spsmail.cuny.edu>
2024-03-03 22:41:01 -0500

    add project 2

--- project_2/Dataset_3_Milk/work.R
+++ project_2/Dataset_3_Milk/work.R
@@ -23,15 +23,14 @@ df$yr<-as.numeric(df$yr)
 # -------- Total Trend: isolate and redefine main categories for trending
 
 df_tidy_wide <- df %>% 
-  select(yr,pop,tot_whole,low_subtot_plain_flav,skim,tot_bev_sales, tot_bev_avail) %>% 
-  mutate(tot_low_skim = low_subtot_plain_flav+skim, 
-         tot_other = tot_bev_avail - tot_whole - tot_low_skim,
+  select(yr,pop,tot_whole,tot_low_skim_all,tot_bev_sales, tot_bev_avail) %>% 
+  mutate(tot_other = tot_bev_avail - tot_whole - tot_low_skim_all,
          tot_consumed_on_site = tot_bev_avail - tot_bev_sales) %>% 
-  select(yr,pop,tot_whole,tot_low_skim,tot_other,tot_bev_avail,tot_bev_sales,tot_consumed_on_site)
+  select(yr,pop,tot_whole,tot_low_skim_all,tot_other,tot_bev_avail,tot_bev_sales,tot_consumed_on_site)
 
 # check totals
 test <- df_tidy_wide %>% 
-  summarize(sum(tot_whole), sum(tot_low_skim), sum(tot_other), sum(tot_consumed_on_site),
+  summarize(sum(tot_whole), sum(tot_low_skim_all), sum(tot_other), sum(tot_consumed_on_site),
             sum(tot_bev_avail), sum(tot_bev_sales))
 
 # check str
@@ -48,12 +47,21 @@ df_tidy <- df_tidy_wide %>%
 
 # -------- line trend #1 whole vs total
 
-df_avail_type <- filter(df_tidy, category %in% c("tot_whole", "tot_low_skim","tot_other"))
+df_avail_type <- filter(df_tidy, category %in% c("tot_whole", "tot_low_skim_all","tot_other"))
+
+
+df_type_sum <- filter(df_avail_type, yr %in% c(1920,1940,1960,1980,2000,2020)) %>% 
+  pivot_wider(names_from = category,values_from = gallons) %>% 
+  mutate(percent_whole = tot_whole/(tot_whole + tot_low_skim_all + tot_other),
+       percent_low_skim = tot_low_skim_all/(tot_whole + tot_low_skim_all + tot_other),
+       percent_other = tot_other/(tot_whole + tot_low_skim_all + tot_other))
+df_type_sum
+
 
 ggplot(df_avail_type, aes(x = yr, y = gallons, fill = category)) +
   geom_bar(stat="identity") +
   scale_fill_brewer(palette = "Paired", name = "Category", 
-                    labels = c("Low Fat or Skim","Other incl. Buttermilk","Whole Milk")
+                    labels = c("Low Fat or Skim/Buttermilk","Other","Whole Milk")
                     ) +
   scale_x_continuous(breaks = breaks_width(10))+
   scale_y_continuous(n.breaks=20, labels = scales::label_comma()) +
@@ -67,6 +75,13 @@ ggplot(df_avail_type, aes(x = yr, y = gallons, fill = category)) +
 
 df_avail_con <- filter(df_tidy, category %in% c("tot_consumed_on_site","tot_bev_sales"))
 
+df_avail_sum <- filter(df_avail_con, yr %in% c(1920,1940,1960,1980,2000,2020)) %>% 
+  pivot_wider(names_from = category,values_from = gallons) %>% 
+  mutate(percent_consumed_on_site = tot_consumed_on_site/(tot_consumed_on_site + tot_bev_sales))
+
+df_avail_sum
+
+
 ggplot(df_avail_con, aes(x = yr, y = gallons, fill = category)) +
   geom_bar(stat="identity") +
   scale_fill_brewer(palette = "Paired", name = "Category", labels = c("Total Available for Sale","Consumed Where Produced")) +
@@ -77,4 +92,29 @@ ggplot(df_avail_con, aes(x = yr, y = gallons, fill = category)) +
   ggtitle("Total Gallons Per Capita: Liquid Milk Availability by Location Consumed")
 
 
+# -------- loss adjustment
+df_loss_pct <- read_csv("https://raw.githubusercontent.com/AmandaSFox/DATA607/main/project_2/Dataset_3_Milk/loss_pct.csv")
+
+df_avail_adj <- filter(df_avail_con, yr %in% c(1970,1980,1990,2000,2010,2020)) %>% 
+  pivot_wider(names_from = category,values_from = gallons) %>%  
+  mutate(tot_avail = tot_consumed_on_site + tot_bev_sales) %>% 
+  left_join(df_loss_pct) %>% 
+  mutate(pct_loss = pct_loss/100, loss_adjusted_avail = (1-pct_loss) * tot_avail) 
+  
+df_avail_adj 
+
+# Create plot: 
+
+ggplot(df_avail_adj, aes(x = yr, y = loss_adjusted_avail)) +
+  geom_bar(stat="identity", fill = "lightblue") +
+  scale_x_continuous(breaks = breaks_width(10))+
+  scale_y_continuous(n.breaks=10, labels = scales::label_comma()) +
+  xlab("Year") +
+  ylab("Total Gallons Per Capita") +
+  ggtitle("Total Gallons Per Capita: Liquid Milk Estimated Consumption (Loss-Adjusted Availability)")
+
+
+
+
+select(df_avail_sum, c(yr, tot_avail,loss_adjusted_avail))
 
