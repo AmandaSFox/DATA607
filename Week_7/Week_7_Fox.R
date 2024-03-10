aa803e07c9323f9e55b2775c03d97300f44ffac9
Amanda Fox <Amanda.Fox02@spsmail.cuny.edu>
2024-03-10 18:42:53 -0400

    week_7

--- Week_7/Week_7_Fox.R
+++ Week_7/Week_7_Fox.R
@@ -1,15 +1,25 @@
 library(tidyverse)
 library(jsonlite)
-
-
+library(RCurl)
+library(XML)
 
 path_json <- "https://raw.githubusercontent.com/AmandaSFox/DATA607/main/Week_7/Books.json"
-path_xml <- "https://raw.githubusercontent.com/AmandaSFox/DATA607/main/Week_7/Books.xml"
-path_html <- "https://raw.githubusercontent.com/AmandaSFox/DATA607/main/Week_7/Books.html"
+# path_xml <- "https://raw.githubusercontent.com/AmandaSFox/DATA607/main/Week_7/Books.xml"
+# path_html <- "https://raw.githubusercontent.com/AmandaSFox/DATA607/main/Week_7/Books.html"
 
 
 df_json <- jsonlite::fromJSON(path_json)
 
-df_xml <- 
+df_json
 
-df_html <- 
+#--------
+
+path_xml <- "https://raw.githubusercontent.com/AmandaSFox/DATA607/main/Week_7/Books.xml"
+
+data_xml <- getURL(path_xml) %>% 
+  xmlParse()
+
+list_xml<-xmlToList(data_xml) 
+df_xml<-xmlToDataFrame(list_xml) 
+
+df_xml
