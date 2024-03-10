82e9ae08b3f9c83c775a6ff3ef1bdba7f5f8eb5b
Amanda Fox <Amanda.Fox02@spsmail.cuny.edu>
2024-03-10 16:24:05 -0400

    xml

---
+++ Week_7/Week_7_Fox.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(jsonlite)
+
+
+
+path_json <- "https://raw.githubusercontent.com/AmandaSFox/DATA607/main/Week_7/Books.json"
+path_xml <- "https://raw.githubusercontent.com/AmandaSFox/DATA607/main/Week_7/Books.xml"
+path_html <- "https://raw.githubusercontent.com/AmandaSFox/DATA607/main/Week_7/Books.html"
+
+
+df_json <- jsonlite::fromJSON(path_json)
+
+df_xml <- 
+
+df_html <- 
